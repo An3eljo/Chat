767dedfb896db319c4fd5d6cec6e39cda775ba54
andrijoos <andri.joos@edu.tbz.ch>
2018-07-31 00:26:01 +0200

    create

---
+++ ChatServer/ChatServer/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChatServer.App_Start.Startup))]
+
+namespace ChatServer.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ ChatServer/ChatServer/MainChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChatServer.Interfaces;
+
+namespace ChatServer
+{
+    public class MainChatHub : IHub
+    {
+        public override void Send()
+        {
+            
+        }
+    }
+}
